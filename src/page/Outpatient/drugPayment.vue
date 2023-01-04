3cb4e662a68045436bc5720c0753212b9e66aa14
wlt <857582847@qq.com>
2023-01-05 01:11:49 +0800

    药品缴费和项目审查

--- src/page/Outpatient/drugPayment.vue
+++ src/page/Outpatient/drugPayment.vue
@@ -1,16 +1,61 @@
 <template>
-	<div>
-		药品缴费
-	</div>
+  <div style="margin: 1.25rem;">
+    <div style="display: inline-block;">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span style="font-size: 20px;">2023-01-05 00:45:09 的药单</span>
+          <el-button style="float: right;" type="primary">缴费</el-button>
+        </div>
+
+        <div v-for="o in 4" :key="o" >
+          <el-row :gutter="20">
+            <el-col :span="20">
+              <div class="grid-content bg-purple">
+                {{'药品 ' + o }}
+              </div>
+            </el-col>
+            <el-col :span="4">
+              <div class="grid-content bg-purple">
+                x{{o}}
+              </div>
+            </el-col>
+          </el-row>
+          <div slot="header">
+            <span></span>
+            <span style="float: right;"></span>
+          </div>
+        </div>
+      </el-card>
+    </div>
+  </div>
 
 </template>
 
 <script>
 export default {
-  name: "drugPayment"
+  name: "drugPayment",
 }
 </script>
 
 <style scoped>
+.text {
+  font-size: 14px;
+}
 
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+
+.box-card {
+  width: 480px;
+}
 </style>
--- src/page/Outpatient/projectCheck.vue
+++ src/page/Outpatient/projectCheck.vue
@@ -1,5 +1,51 @@
 <template>
-	<div>项目检查</div>
+  <div style="margin-top:20px">
+    <div style="font-size: 1.5rem; display: inline-block; margin-right: 2rem;">所属科室：血液科</div>
+    <div style="font-size: 1.5rem; display: inline-block;">在岗医生：张三</div>
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          prop="date"
+          label="患者姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="患者年龄"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="患者性别"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="检验项目"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+          width="200"
+          align="left">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button
+                type="primary"
+                icon="el-icon-edit"
+                size="mini"
+                @click="edit(scope)"
+            >
+              修改
+            </el-button>
+
+          </el-button-group>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -9,5 +55,11 @@ export default {
 </script>
 
 <style scoped>
-
+.box{
+  margin-bottom: 1.8rem;
+}
+.name,.value{
+  font-size: 30px;
+  display: inline-block;
+}
 </style>
